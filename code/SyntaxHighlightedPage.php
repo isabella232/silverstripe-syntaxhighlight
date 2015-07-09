c9a330b9398a3418b69f4dd764f8ebf68dce2489
Nara Kasbergen <bkasbergen@npr.org>
2015-07-09 15:32:51 -0400

    Seeing if we can use Requirements::combine_files to reduce the number of HTTP requests

--- code/SyntaxHighlightedPage.php
+++ code/SyntaxHighlightedPage.php
@@ -4,15 +4,24 @@ class SyntaxHighlightedPage extends DataExtension
 {
     function contentcontrollerInit()
     {
-        Requirements::javascript("framework/thirdparty/jquery/jquery.js");
+        Requirements::javascript('framework/thirdparty/jquery/jquery.js');
 
-        Requirements::css(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/styles/shCore.css");
-        Requirements::css(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/styles/shThemeDefault.css");
+        Requirements::combine_files('syntaxhighlighter.css',
+            [
+                SYNTAX_DIR . '/thirdparty/syntaxhighlighter/styles/shCore.css',
+                SYNTAX_DIR . '/thirdparty/syntaxhighlighter/styles/shThemeDefault.css'
+            ], 'screen'
+        );
 
-        Requirements::javascript(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/scripts/XRegExp.js");
-        Requirements::javascript(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/scripts/shCore.js");
-        Requirements::javascript(SYNTAX_DIR . "/thirdparty/syntaxhighlighter/scripts/shAutoloader.js");
-        Requirements::javascript(SYNTAX_DIR . "/js/syntaxhighlighter-plugin.js");
+        Requirements::combine_files('syntaxhighlighter.js',
+            [
+                SYNTAX_DIR . '/thirdparty/syntaxhighlighter/scripts/XRegExp.js',
+                SYNTAX_DIR . '/thirdparty/syntaxhighlighter/scripts/shCore.js',
+                SYNTAX_DIR . '/thirdparty/syntaxhighlighter/scripts/shAutoloader.js',
+                SYNTAX_DIR . '/js/syntaxhighlighter-plugin.js'
+            ]
+        );
+
         $syntax_dir = SYNTAX_DIR;
         Requirements::customScript(<<<JS
     (function ($) {
